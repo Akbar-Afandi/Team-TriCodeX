636a5b1adb08da918499cda212cccf4f542ce95d
Akbar Afandi <greatemperorvoid@gmail.com>
2025-09-04 10:16:10 +0700

    y

--- assets/js/header.js
+++ assets/js/header.js
@@ -7,35 +7,28 @@ document.addEventListener("DOMContentLoaded", () => {
   const galeri = document.getElementById("navgaleri")
   const tim = document.getElementById("navtim")
   const suit = document.getElementById("navsuit")
+  const lNavGame = document.getElementById("listnavgame")
+  const arrowNavGame = document.getElementById("navcepron")
+  const pGame = document.getElementById("pNavGame")
+  const navSortir = document.getElementById("navsortir")
   
+  
+  
+
   
   bar.addEventListener("click", () => {
     menu.classList.remove("ilang")
     menu.classList.add("muncul")
+    bar.style.opacity = "0"
   })
   
   exit.addEventListener("click", () => {
     menu.classList.remove("muncul")
     menu.classList.add("ilang")
+    bar.style.opacity = "1"
   })
   
   home.addEventListener("click", () => {
     window.location.assign("index.html")
-  })
-  
-  tipsaksi.addEventListener("click", () => {
-    window.location.assign("tips-aksi.html")
-  })
-  
-  galeri.addEventListener("click", () => {
-    window.location.assign("gallery.html")
-  })
-  
-  tim.addEventListener("click", () => {
-    window.location.assign("ourteam.html")
-  })
-  
-  suit.addEventListener("click", () => {
-    window.location.assign("suit.html")
   })
 })
